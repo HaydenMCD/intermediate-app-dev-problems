0992ba3290e09add84ef7aac90f1d1ff117b5f6d
Hayden-McD <mcdohr2@student.op.ac.nz>
2021-09-28 15:30:50 +1300

    Added a sleep of 2 seconds into line 65 as it was missing

--- 01-Stacks/01-Stacks.py
+++ 01-Stacks/01-Stacks.py
@@ -1,5 +1,6 @@
 import time
 
+
 class Stack:
     def __init__(self):
         self.items = []
@@ -18,47 +19,56 @@ class Stack:
 
     def size(self):
         return len(self.items)
-    
+
     def wipe(self):
+        # Loops thorugh the stack removing each item until the stack is empty
         while (not self.items == []):
             s.pop()
 
-s=Stack()
-print('Is the stack currently empty?') #Testing if the stack is empty. True = empty. False = not empty.
+
+s = Stack()
+# Testing if the stack is empty. True = empty. False = not empty.
+print('Is the stack currently empty?')
 print(s.is_empty())
 time.sleep(2)
 print('')
 print('---------------------------------------------------------------------------')
 print('')
 
-print('Now some data will be added to the stack. The number 4 has been added') #Adding '4' to the stack.
+# Adding '4' to the stack.
+print('Now some data will be added to the stack. The number 4 has been added')
 time.sleep(2)
 s.push(4)
 print('')
 print('---------------------------------------------------------------------------')
 print('')
 
-print('Is the stack currently empty?') #Testing if the stack is empty. True = empty. False = not empty.
+# Testing if the stack is empty. True = empty. False = not empty.
+print('Is the stack currently empty?')
 print(s.is_empty())
 time.sleep(2)
 print('')
 print('---------------------------------------------------------------------------')
 print('')
 
-print('Now you can see that the last number added to the stack is:') #Displaying the data ontop of the stack.
+# Displaying the data ontop of the stack.
+print('Now you can see that the last number added to the stack is:')
 print(s.peek())
 time.sleep(2)
 print('')
 print('---------------------------------------------------------------------------')
 print('')
 
-print('and that the size of the stack is now: ') #Displaying the size of the whole stack.
+# Displaying the size of the whole stack.
+print('and that the size of the stack is now: ')
 print(s.size())
+time.sleep(2)
 print('')
 print('---------------------------------------------------------------------------')
 print('')
 
-print('Now I will add the numbers 5, 3 ,1 and 2') #Adding '5', '3', '1' and '2' to the stack.
+# Adding '5', '3', '1' and '2' to the stack.
+print('Now I will add the numbers 5, 3 ,1 and 2')
 s.push(5)
 s.push(3)
 s.push(1)
@@ -68,56 +78,56 @@ print('')
 print('---------------------------------------------------------------------------')
 print('')
 
-print('Now the new numbers have been added the stack last number in the stack is:') #
+print('Now the new numbers have been added the stack last number in the stack is:')
 print(s.peek())
 time.sleep(2)
 print('')
 print('---------------------------------------------------------------------------')
 print('')
 
-print('and the stack size is now:') #
+print('and the stack size is now:')
 print(s.size())
 time.sleep(2)
 print('')
 print('---------------------------------------------------------------------------')
 print('')
 
-print('Now I will delete the last number in the stack') #
+print('Now I will delete the last number in the stack')
 s.pop()
 time.sleep(2)
 print('')
 print('---------------------------------------------------------------------------')
 print('')
 
-print('Now that the last number has been deleted, the new last number is:') #
+print('Now that the last number has been deleted, the new last number is:')
 print(s.peek())
 time.sleep(2)
 print('')
 print('---------------------------------------------------------------------------')
 print('')
 
-print('and the stack size is now:') #
+print('and the stack size is now:')
 print(s.size())
 time.sleep(2)
 print('')
 print('---------------------------------------------------------------------------')
 print('')
 
-print('I will now delete all of the remaining numbers') #
+print('I will now delete all of the remaining numbers')
 s.wipe()
 time.sleep(2)
 print('')
 print('---------------------------------------------------------------------------')
 print('')
 
-print('The stack size is now:') #
+print('The stack size is now:')
 print(s.size())
 time.sleep(2)
 print('')
 print('---------------------------------------------------------------------------')
 print('')
 
-print('and the stack is now empty:') #
+print('and the stack is now empty:')
 print(s.is_empty())
 time.sleep(2)
 print('')
