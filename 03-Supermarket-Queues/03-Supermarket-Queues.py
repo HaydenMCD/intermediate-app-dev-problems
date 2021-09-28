b11567eed14ee924d5b553656a4618a391fe1212
Hayden-McD <mcdohr2@student.op.ac.nz>
2021-09-28 20:29:23 +1300

    Added files for question 3 and question 4

---
+++ 03-Supermarket-Queues/03-Supermarket-Queues.py
@@ -0,0 +1,25 @@
+def __init__(self):
+    queue = []
+
+
+def enqueue():
+    pass
+
+
+def dequeue():
+    pass
+
+
+def front():
+    pass
+
+
+def rear():
+    pass
+
+def createShoppers():
+    pass
+
+
+class Shopper:
+    checkout_time = None
